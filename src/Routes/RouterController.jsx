bdc52bac50ff86a5a7ede004f9fac13d70de330b
RamiroFerradas <ramiferra97@gmail.com>
2023-03-12 17:37:20 -0300

    bug rutas

--- src/Routes/RouterController.jsx
+++ src/Routes/RouterController.jsx
@@ -1,44 +1,44 @@
-import { Navbar } from "@material-tailwind/react";
-import { Route, Routes } from "react-router-dom";
-import Landing from "../Components/Landing/Landing";
-import Navmenu from "../Components/Navmenu/Navmenu";
-import HomeCreateTeam from "../Pages/CreateTeam/Components/Home/HomeCreateTeam";
-import HomeMatch from "../Pages/Match/Components/Home/HomeMatch";
-import HomeTeams from "../Pages/Teams/Components/Home/HomeTeams";
+// import { Navbar } from "@material-tailwind/react";
+// import { Route, Routes } from "react-router-dom";
+// import Landing from "../Components/Landing/Landing";
+// import Navmenu from "../Components/Navmenu/Navmenu";
+// import HomeCreateTeam from "../Pages/CreateTeam/Components/Home/HomeCreateTeam";
+// import HomeMatch from "../Pages/Match/Components/Home/HomeMatch";
+// import HomeTeams from "../Pages/Teams/Components/Home/HomeTeams";
 
-function RouterController() {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route
-        path="/create"
-        element={
-          <>
-            <Navmenu />
-            <HomeCreateTeam />
-          </>
-        }
-      />
-      <Route
-        path="/teams"
-        element={
-          <>
-            <Navmenu />
-            <HomeTeams />
-          </>
-        }
-      />
-      <Route
-        path="/match"
-        element={
-          <>
-            <Navmenu />
-            <HomeMatch />
-          </>
-        }
-      />
-    </Routes>
-  );
-}
+// function RouterController() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Landing />} />
+//       <Route
+//         path="/create"
+//         element={
+//           <>
+//             <Navmenu />
+//             <HomeCreateTeam />
+//           </>
+//         }
+//       />
+//       <Route
+//         path="/teams"
+//         element={
+//           <>
+//             <Navmenu />
+//             <HomeTeams />
+//           </>
+//         }
+//       />
+//       <Route
+//         path="/match"
+//         element={
+//           <>
+//             <Navmenu />
+//             <HomeMatch />
+//           </>
+//         }
+//       />
+//     </Routes>
+//   );
+// }
 
-export { RouterController };
+// export { RouterController };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./Components/Landing/Landing";
+import Navmenu from "./Components/Navmenu/Navmenu";
 import { TeamsProvider } from "./Context/TeamsContext";
 
 import "./index.css";
-import { RouterController } from "./Routes/RouterController";
+import HomeCreateTeam from "./Pages/CreateTeam/Components/Home/HomeCreateTeam";
+import HomeMatch from "./Pages/Match/Components/Home/HomeMatch";
+import HomeTeams from "./Pages/Teams/Components/Home/HomeTeams";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TeamsProvider>
       <BrowserRouter>
-        <RouterController />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/create"
+            element={
+              <>
+                <Navmenu />
+                <HomeCreateTeam />
+              </>
+            }
+          />
+          <Route
+            path="/teams"
+            element={
+              <>
+                <Navmenu />
+                <HomeTeams />
+              </>
+            }
+          />
+          <Route
+            path="/match"
+            element={
+              <>
+                <Navmenu />
+                <HomeMatch />
+              </>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </TeamsProvider>
   </React.StrictMode>
