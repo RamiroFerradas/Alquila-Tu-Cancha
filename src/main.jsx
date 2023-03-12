b23a5f0723fe5f71755180540ff769aa2d040606
RamiroFerradas <ramiferra97@gmail.com>
2023-03-12 17:38:54 -0300

    bug rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landing from "./Components/Landing/Landing";
-import Navmenu from "./Components/Navmenu/Navmenu";
+import { BrowserRouter } from "react-router-dom";
 import { TeamsProvider } from "./Context/TeamsContext";
 
 import "./index.css";
-import HomeCreateTeam from "./Pages/CreateTeam/Components/Home/HomeCreateTeam";
-import HomeMatch from "./Pages/Match/Components/Home/HomeMatch";
-import HomeTeams from "./Pages/Teams/Components/Home/HomeTeams";
+import { RouterController } from "./Routes/RouterController";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TeamsProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route
-            path="/create"
-            element={
-              <>
-                <Navmenu />
-                <HomeCreateTeam />
-              </>
-            }
-          />
-          <Route
-            path="/teams"
-            element={
-              <>
-                <Navmenu />
-                <HomeTeams />
-              </>
-            }
-          />
-          <Route
-            path="/match"
-            element={
-              <>
-                <Navmenu />
-                <HomeMatch />
-              </>
-            }
-          />
-        </Routes>
+        <RouterController />
       </BrowserRouter>
     </TeamsProvider>
   </React.StrictMode>
