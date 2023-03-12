9ecbfe83398c1d291c34cffa4603ef25b2b23ba7
RamiroFerradas <ramiferra97@gmail.com>
2023-03-12 16:51:21 -0300

    bug detalles

--- src/Pages/CreateTeam/Components/Players/Details.jsx
+++ src/Pages/CreateTeam/Components/Players/Details.jsx
@@ -21,122 +21,121 @@ export default function Details({
 }) {
   let { data, loading } = useFetch(PLAYER_URL(playerName));
 
-  console.log(data);
   const ratings = data?.map((player) => {
     const rating = parseInt(player.player_rating);
     return isNaN(rating) ? 0 : rating;
   });
-  console.log(ratings);
+
   const averageRating =
     ratings !== undefined
       ? ratings?.reduce((total, rating) => total + rating, 0) / ratings?.length
       : 0;
 
-  console.log(averageRating);
-
   const teamNamesSet = new Set(data?.map((ele) => ele.team_name));
 
   return (
     <Fragment>
-      <Dialog
-        open={openDetail}
-        handler={(e) => {
-          e.stopPropagation();
-          setOpenDetail(!openDetail);
-        }}
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0.9, y: -100 },
-        }}
-        className=" overflow-auto rounded-3xl bg-gray-900 bg-opacity-90 backdrop-blur-xs"
-      >
-        <DialogBody>
-          <div
-            key={data?.[0].player_name}
-            className="flex flex-col items-center justify-center  border-warning-300 border-4 rounded-3xl overflow-hidden h-[25rem] text-white"
-            style={{
-              background:
-                "linear-gradient(to right, rgba(34, 34, 35, 0.797), rgba(167, 167, 167, 0.1))",
-            }}
-          >
-            <button
-              className="absolute top-0 right-0 p-2 "
-              onClick={(e) => {
-                e.stopPropagation(e);
-                setOpenDetail(false);
+      <div className="">
+        <Dialog
+          open={openDetail}
+          handler={(e) => {
+            e.stopPropagation();
+            setOpenDetail(!openDetail);
+          }}
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0.9, y: -100 },
+          }}
+          className="min-w-full md:min-w-0 overflow-auto rounded-3xl bg-gray-900 bg-opacity-90 backdrop-blur-xs"
+        >
+          <DialogBody>
+            <div
+              key={data?.[0].player_name}
+              className="flex flex-col items-center justify-center  border-warning-300 border-4 rounded-3xl overflow-hidden h-[25rem] text-white"
+              style={{
+                background:
+                  "linear-gradient(to right, rgba(34, 34, 35, 0.797), rgba(167, 167, 167, 0.1))",
               }}
             >
-              <p className="text-xl">
-                <FaWindowClose />
-              </p>
-            </button>
-            {loading ? (
-              <Loader />
-            ) : (
-              <div className="flex flex-col items-center relative">
-                <div className="absolute left-5 top-4 text-lg flex">
-                  <p>⭐</p>
-                  <p className="text-2xl font-bold ">
-                    {Math.ceil(averageRating * 10)}
-                  </p>
-                </div>
-                <div>
-                  <img
-                    className=" rounded-3xl object-cover mb-4 h-48 pt-1"
-                    src={playerImage}
-                    alt={data?.[0].player_name}
-                    onError={(e) => {
-                      e.target.onerror = null;
-                      e.target.src = player_unknown;
-                    }}
-                  />
-                </div>
-                <p className="text-2xl font-bold mb-2">
-                  {data?.[0].player_name}
+              <button
+                className="absolute top-0 right-0 p-2 "
+                onClick={(e) => {
+                  e.stopPropagation(e);
+                  setOpenDetail(false);
+                }}
+              >
+                <p className="text-xl">
+                  <FaWindowClose />
                 </p>
-                <div className="grid grid-cols-2 gap-12">
-                  <div className="">
-                    <div className="flex flex-col items-center  p-4">
-                      <p className="text-md">
-                        <span className="font-semibold mr-2">Edad:</span>
-                        {data?.[0].player_age}
-                      </p>
-                      <p className="text-md"></p>
-                      <span className="font-semibold mr-2">Nacimiento:</span>
-                      {data?.[0].player_birthdate}
-                      <p className="text-md">
-                        <span className="font-semibold mr-2">Posicion</span>
-                        {data?.[0].player_type.slice()}
-                      </p>
+              </button>
+              {loading ? (
+                <Loader />
+              ) : (
+                <div className="flex flex-col items-center relative">
+                  <div className="absolute left-5 top-4 text-lg flex">
+                    <p>⭐</p>
+                    <p className="text-2xl font-bold ">
+                      {Math.ceil(averageRating * 10)}
+                    </p>
+                  </div>
+                  <div>
+                    <img
+                      className=" rounded-3xl object-cover mb-4 h-48 pt-1"
+                      src={playerImage}
+                      alt={data?.[0].player_name}
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = player_unknown;
+                      }}
+                    />
+                  </div>
+                  <p className="text-2xl font-bold mb-2">
+                    {data?.[0].player_name}
+                  </p>
+                  <div className="grid grid-cols-2 gap-12">
+                    <div className="">
+                      <div className="flex flex-col items-center  p-4">
+                        <p className="text-md">
+                          <span className="font-semibold mr-2">Edad:</span>
+                          {data?.[0].player_age}
+                        </p>
+                        <p className="text-md"></p>
+                        <span className="font-semibold mr-2">Nacimiento:</span>
+                        {data?.[0].player_birthdate}
+                        <p className="text-md">
+                          <span className="font-semibold mr-2">Posicion</span>
+                          {data?.[0].player_type.slice()}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="overflow-auto h-40">
+                      <p className="text-md font-semibold">Participaciones:</p>
+                      <ul>
+                        {[...teamNamesSet].map((teamName, index) => (
+                          <li key={index}>{`- ${teamName}`}</li>
+                        ))}
+                      </ul>
                     </div>
                   </div>
-                  <div className="overflow-auto h-40">
-                    <p className="text-md font-semibold">Participaciones:</p>
-                    <ul>
-                      {[...teamNamesSet].map((teamName, index) => (
-                        <li key={index}>{`- ${teamName}`}</li>
-                      ))}
-                    </ul>
-                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        </DialogBody>
-        <DialogFooter className="justify-center flex-row">
-          <Button
-            variant="gradient"
-            color="green"
-            onClick={(e) => {
-              e.stopPropagation();
-              selectTeam(e, data?.[0]);
-            }}
-            className={!data && `hidden`}
-          >
-            <span>Agregar a equipo</span>
-          </Button>
-        </DialogFooter>
-      </Dialog>
+              )}
+            </div>
+          </DialogBody>
+          <DialogFooter className="justify-center flex-row">
+            <Button
+              variant="gradient"
+              color="green"
+              onClick={(e) => {
+                e.stopPropagation();
+                selectTeam(e, data?.[0]);
+              }}
+              className={!data && `hidden`}
+            >
+              <span>Agregar a equipo</span>
+            </Button>
+          </DialogFooter>
+        </Dialog>
+      </div>
     </Fragment>
   );
 }
